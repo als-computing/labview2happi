d1f3397818f043fdcb16975c91a4e4c175eca650
Juliane Reinhardt <jreinhardt@lbl.gov>
2021-03-23 12:54:31 -1000

    change ip

--- wipe_labview_collection.py
+++ wipe_labview_collection.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 USERNAME = os.getenv("USERNAME_MONGO")
 PASSWD = os.getenv("USERNAME_MONGO")
 #connect to mongoDB on tsuru
-client =  MongoClient(f'mongodb://{USERNAME}:{PASSWD}@131.243.73.51:27017/happi?authsource=happi')
+client =  MongoClient(f'mongodb://{USERNAME}:{PASSWD}@131.243.73.172:27017/happi?authsource=happi')
 #TODO: add authentication
 db = client['happi']
 collection = db['labview']
