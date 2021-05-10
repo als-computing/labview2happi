3c4c103e69b9343192ccd14b1d8fd9803588cdce
Juliane <jreinhardt@lbl.gov>
2021-05-10 14:35:14 -0700

    update add_single_entry

--- add_single_entry.py
+++ add_single_entry.py
@@ -1,13 +1,11 @@
-import os
-import re
 import sys
+import warnings
 from dotenv import dotenv_values
-
 from happi.backends.mongo_db import MongoBackend
 from happi import Client
 from happi.errors import EntryError, DuplicateError
 
-
+print(sys.argv[1])
 conf = dotenv_values(sys.argv[1])
 
 USER = conf.get("USER_HAPPI")
@@ -15,14 +13,9 @@ PASSWD = conf.get("PASSWD_HAPPI")
 HOST = conf.get("HOST_HAPPI")
 DB = conf.get("DB_HAPPI")
 COLLECTION = conf.get("COLLECTION_HAPPI")
-PREFIX = conf.get("PREFIX_HAPPI")
+# PREFIX = conf.get("PREFIX_HAPPI")
+PREFIX = "SR"
 BEAMLINE = conf.get("BEAMLINE_HAPPI")
-EXCLUDE_LIST_PATH = conf.get("EXCLUDE_LIST_HAPPI")
-
-exclude_file = open(EXCLUDE_LIST_PATH, 'r')
-exclude_dev_list = []
-for line in exclude_file:
-    exclude_dev_list.append(line.strip())
 
 db = MongoBackend(host=HOST,
                   db=DB,
@@ -31,17 +24,21 @@ db = MongoBackend(host=HOST,
                   collection=COLLECTION,
                   timeout=None)
 
-device_name = "TestDevice"
-source = "test"
+try:
+    device_name = sys.argv[2]
+    source = sys.argv[3]
+except:
+    warnings.warn("No device name or source given as arguments. "
+                  "Please run 'python add_single_entry.py $path_to_conv.env_file $device_name $source_name' ")
 
 client = Client(db)
 device = client.create_device("Device",
-                                   name=device_name,
-                                   prefix=f"{PREFIX}:{device_name}",
-                                   beamline="ALS beamline",
-                                   location_group="Loc1",
-                                   functional_group="Func1",
-                                   device_class="ophyd.EpicsMotor",
-                                   args=["{{prefix}}"],
-                                   source=source)
+                              name=device_name,
+                              prefix=f"{PREFIX}:{device_name}",
+                              beamline="ALS beamline",
+                              location_group="Loc1",
+                              functional_group="Func1",
+                              device_class="ophyd.EpicsMotor",
+                              args=["{{prefix}}"],
+                              source=source)
 device.save()
